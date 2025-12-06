b0d49d07480bf29929020a71f67b7c79719e9c57
Juliorubiodev <juliorubiodev@gmail.com>
2025-12-06 03:50:07 +0100

    Deploy to Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
+  base: "/JRDev_Portfolio/",
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
